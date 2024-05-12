45021f15faef0581db678e2718a7d4e6a5abab3d
Elarbi Allam <elarbi.allam.51@gmail.com>
2024-05-12 20:53:49 +0100

    Models Version 1

--- api/app/Models/Facture.php
+++ api/app/Models/Facture.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Facture extends Model
 {
     use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [];
+
+    /**
+     * Get the devis associated with the facture.
+     */
+    public function devis(): HasMany
+    {
+        return $this->hasMany(Devis::class);
+    }
+
+
 }
